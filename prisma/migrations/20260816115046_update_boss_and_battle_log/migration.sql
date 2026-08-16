ea730a24700b270ca1f24bc79d72f30175b54083
권지윤 <kjyoon071025@gmail.com>
2026-08-16 20:56:49 +0900

    feat: Boss, BattleLog 스키마 수정 및 DB 마이그레이션 적용

---
+++ prisma/migrations/20260816115046_update_boss_and_battle_log/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_critical` on the `battle_logs` table. All the data in the column will be lost.
+  - Added the required column `elapsed_ms` to the `battle_logs` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `started_at` to the `battle_logs` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `touch_count` to the `battle_logs` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "battle_logs" DROP COLUMN "is_critical",
+ADD COLUMN     "battle_type" VARCHAR(30) NOT NULL DEFAULT 'BOSS_TIMED',
+ADD COLUMN     "critical_count" INTEGER NOT NULL DEFAULT 0,
+ADD COLUMN     "elapsed_ms" INTEGER NOT NULL,
+ADD COLUMN     "is_cleared" BOOLEAN NOT NULL DEFAULT false,
+ADD COLUMN     "started_at" TIMESTAMP(3) NOT NULL,
+ADD COLUMN     "touch_count" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "bosses" ADD COLUMN     "cutout_image_url" TEXT,
+ADD COLUMN     "time_limit_ms" INTEGER;
+
+-- AlterTable
+ALTER TABLE "monsters" ADD COLUMN     "cutout_image_url" TEXT;
